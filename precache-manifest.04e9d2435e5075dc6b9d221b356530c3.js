f12ae9a01c21c309f42d61520550ad18ad7aa95a
Somsubhra Das <das.somsubhra1@gmail.com>
2019-06-26 13:14:17 +0530

    Updates

---
+++ precache-manifest.04e9d2435e5075dc6b9d221b356530c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7f6da5bf8ee52c87153e177d6cf9f1",
+    "url": "/Recipe-Finder-App/index.html"
+  },
+  {
+    "revision": "81454fc9cee162819fcb",
+    "url": "/Recipe-Finder-App/static/css/main.a5ce0803.chunk.css"
+  },
+  {
+    "revision": "dde5bf53dcde63ec9393",
+    "url": "/Recipe-Finder-App/static/js/2.db3d7f2b.chunk.js"
+  },
+  {
+    "revision": "81454fc9cee162819fcb",
+    "url": "/Recipe-Finder-App/static/js/main.0c8aeeba.chunk.js"
+  },
+  {
+    "revision": "ae15263fda2d21dfe835",
+    "url": "/Recipe-Finder-App/static/js/runtime~main.2b5df883.js"
+  },
+  {
+    "revision": "8e9c6f69f1b0c42ed46f99ac92514ad3",
+    "url": "/Recipe-Finder-App/static/media/spinner.8e9c6f69.gif"
+  }
+]);
